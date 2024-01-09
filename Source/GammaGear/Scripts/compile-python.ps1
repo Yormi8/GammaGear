145d368ca3edd254989fd4c00a88011a7b0cb88f
Yormi8 <126372786+Yormi8@users.noreply.github.com>
2024-01-08 23:24:31 -0500

    repo: fix python build script

--- Source/GammaGear/Scripts/compile-python.ps1
+++ Source/GammaGear/Scripts/compile-python.ps1
@@ -1,11 +1,18 @@
 param([String]$outpath, [String]$inputs)
 
 $projects = $inputs.split(";")
+
 foreach ($p in $projects)
 {
+    # Get the most recently edited file's write time
+    $ticks = (Get-ChildItem $p -Recurse -File | Sort-Object -Descending -Property LastWriteTime | select -First 1).LastWriteTime.Ticks.ToString()
+
     $folder = Get-Item $p
     $name = $folder.Name
-    if ((Test-Path "$outpath\$name.timestamp") -and ($folder.LastWriteTime.Ticks.ToString() -eq (Get-Content "$outpath\$name.timestamp")))
+
+    Write-Host "Building $name"
+
+    if ((Test-Path "$outpath\$name.timestamp") -and ($ticks -eq (Get-Content "$outpath\$name.timestamp")))
     {
         Write-Host "Skipping building $name because up-to-date build was found..."
         continue
@@ -16,7 +23,12 @@ foreach ($p in $projects)
     {
         exit 1
     }
-    New-Item -Path "$outpath" -Name "$name.timestamp" -ItemType "file" -Value $folder.LastWriteTime.Ticks | Out-Null
+
+    if (Test-Path "$outpath\$name.timestamp")
+    {
+        Remove-Item "$outpath\$name.timestamp"
+    }
+    New-Item -Path "$outpath" -Name "$name.timestamp" -ItemType "file" -Value $ticks 2>$null | Out-Null
 
     # Python.Included writes to the appdata/local dir, get rid of the stuff that that it writes if we are updating our packages.
     Remove-Item -Path $env:LOCALAPPDATA\python-3.11.0-embed-amd64 -Recurse 2>$null
