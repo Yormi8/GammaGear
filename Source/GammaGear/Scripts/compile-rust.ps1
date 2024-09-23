a6da9a7ce8253eaca12679cc7858103d493ef168
Yormi8 <126372786+Yormi8@users.noreply.github.com>
2024-09-23 02:40:41 -0700

    Revert "proj: trying out C++ based python interaction"
    This reverts commit 0d6ca182d1dcb2f0a8758656585aa6790925435e.

---
+++ Source/GammaGear/Scripts/compile-rust.ps1
@@ -0,0 +1,25 @@
+﻿param([String]$outpath, [String]$inputs, [String]$config)
+
+$projects = $inputs.split(";")
+foreach ($p in $projects)
+{
+    $folder = Get-Item $p
+    $name = $folder.Name
+
+    Write-Host "Building $name"
+
+    if ($config -eq "Debug")
+    {
+        cargo build --lib --target-dir "$outpath\ggkatsuba" --manifest-path "$p/Cargo.toml"
+    }
+    else
+    {
+        cargo build --lib --target-dir "$outpath\ggkatsuba" --manifest-path "$p/Cargo.toml" --release
+    }
+
+    if (!$?)
+    {
+        exit 1
+    }
+}
+exit 0
