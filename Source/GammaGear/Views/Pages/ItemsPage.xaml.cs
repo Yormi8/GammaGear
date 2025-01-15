78b8eb074b2afd2d0adc6a01bc4ef82ba07ef681
Yormi8 <126372786+Yormi8@users.noreply.github.com>
2025-01-14 21:45:25 -0800

    feat: filter prototype progess

--- Source/GammaGear/Views/Pages/ItemsPage.xaml.cs
+++ Source/GammaGear/Views/Pages/ItemsPage.xaml.cs
@@ -1,3 +1,4 @@
+using GammaGear.Extensions;
 using GammaGear.ViewModels;
 using GammaGear.ViewModels.Pages;
 using GammaItems;
@@ -5,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Wpf.Ui;
 using Wpf.Ui.Controls;
+using Button = Wpf.Ui.Controls.Button;
+using Image = Wpf.Ui.Controls.Image;
 
 namespace GammaGear.Views.Pages
 {
@@ -27,9 +31,99 @@ namespace GammaGear.Views.Pages
     /// </summary>
     public partial class ItemsPage : Page
     {
+        public class FilterButtonInfo
+        {
+            public Uri ImageSource { get; set; }
+            public string Tooltip { get; set; }
+
+            public FilterButtonInfo(string path, string tooltip)
+            {
+                ImageSource = new Uri(@"pack://application:,,,/GammaGear;component/" + path);
+                Tooltip = tooltip;
+            }
+        }
+        public class FilterSchoolInfo
+        {
+            public Uri ImageSource { get; set; }
+            public School Name { get; set; }
+
+            public FilterSchoolInfo(School name)
+            {
+                ImageSource = name.ToIconUri();
+                Name = name;
+            }
+        }
+        public class FilterEquipmentInfo
+        {
+            public Uri ImageSource { get; set; }
+            public ItemType Name { get; set; }
+
+            public FilterEquipmentInfo(ItemType name)
+            {
+                ImageSource = name.ToIconUri();
+                Name = name;
+            }
+        }
         public ItemsPageViewModel ViewModel { get; }
         public School FilterSchool { get; set; } = School.None;
         public ItemType FilterItemType { get; set; } = ItemType.None;
+        public List<FilterButtonInfo> FilterOptionButtons { get; set; } = new List<FilterButtonInfo>()
+        {
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Health.png", "Health"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Mana.png", "Mana"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Energy.png", "Energy"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Damage.png", "Damage"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Resistance.png", "Resistance"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Accuracy.png", "Accuracy"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Power_Pip.png", "Powerpip Chance"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Critical.png", "Critical Rating"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Critical_Block.png", "Critical Block Rating"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Armor_Piercing.png", "Armor Piercing"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Pip_Conversion.png", "Pip Conversion"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Shadow_Pip.png", "Shadow Pip Rating"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Stun_Resistance.png", "Stun Resistance"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_SpeedBonus.png", "Speed Bonus"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Fishing_Luck.png", "Fishing Luck"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Archmastery.png", "Archmastery Rating"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Flat_Damage.png", "Flat Damage"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Flat_Resistance.png", "Flat Resistance"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Incoming.png", "Incoming Healing"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Outgoing.png", "Outgoing Healing"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Pip.png", "Extra Pips"),
+            new FilterButtonInfo("Assets/Images/(Icon)_Stats_Power_Pip.png", "Extra Power Pips"),
+        };
+        public List<FilterSchoolInfo> FilterSchools { get; set; } = new List<FilterSchoolInfo>()
+        {
+            new FilterSchoolInfo(School.Universal),
+            new FilterSchoolInfo(School.Fire),
+            new FilterSchoolInfo(School.Ice),
+            new FilterSchoolInfo(School.Storm),
+            new FilterSchoolInfo(School.Myth),
+            new FilterSchoolInfo(School.Life),
+            new FilterSchoolInfo(School.Death),
+            new FilterSchoolInfo(School.Balance),
+        };
+        public List<FilterEquipmentInfo> FilterItemTypes { get; set; } = new List<FilterEquipmentInfo>()
+        {
+            new FilterEquipmentInfo(ItemType.None),
+            new FilterEquipmentInfo(ItemType.Hat),
+            new FilterEquipmentInfo(ItemType.Robe),
+            new FilterEquipmentInfo(ItemType.Boots),
+            new FilterEquipmentInfo(ItemType.Wand),
+            new FilterEquipmentInfo(ItemType.Athame),
+            new FilterEquipmentInfo(ItemType.Amulet),
+            new FilterEquipmentInfo(ItemType.Ring),
+            new FilterEquipmentInfo(ItemType.Deck),
+            new FilterEquipmentInfo(ItemType.Pet),
+            new FilterEquipmentInfo(ItemType.Mount),
+            new FilterEquipmentInfo(ItemType.TearJewel),
+            new FilterEquipmentInfo(ItemType.CircleJewel),
+            new FilterEquipmentInfo(ItemType.SquareJewel),
+            new FilterEquipmentInfo(ItemType.TriangleJewel),
+            new FilterEquipmentInfo(ItemType.PinSquarePip),
+            new FilterEquipmentInfo(ItemType.PinSquareShield),
+            new FilterEquipmentInfo(ItemType.PinSquareSword),
+        };
 
         public ItemsPage(
             ItemsPageViewModel viewModel,
@@ -42,16 +136,7 @@ namespace GammaGear.Views.Pages
 
             WizItemsView.Items.Filter = ItemsFilter;
 
-            navigationService.GetNavigationControl().Navigating += OnNavigate;
-        }
-
-        private void OnNavigate(NavigationView sender, Wpf.Ui.Controls.NavigatingCancelEventArgs args)
-        {
-            if (args.Page.Equals(this))
-            {
-                // Navigating to this page
-                FilterToggle.IsChecked = false;
-            }
+            //navigationService.GetNavigationControl().Navigating += OnNavigate;
         }
 
         private bool ItemsFilter(object obj)
@@ -80,6 +165,11 @@ namespace GammaGear.Views.Pages
                     ViewModel.SelectedItemChanged(item);
                 }
             }
+        }
+
+        private void FilterFlyoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            FilterFlyout.IsOpen = true;
         }
     }
 }
